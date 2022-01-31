284ee530ae73667d35738aa9e1015538bc4b8762
Carlos Matos <carlosmatos13@gmail.com>
2022-01-30 23:00:30 -0300

    Fix more responsive

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
   import type { Product } from '../types';
+  import { cart } from '../stores';
   export let product: Product;
+
+  const handleAddToCart = (): void => {
+    $cart = [...$cart, product];
+  };
 </script>
 
 <div
@@ -21,33 +26,39 @@
       >
         {product.description}
       </p>
-      <div class="flex flex-row mt-2">
+      <div class="flex mt-2 justify-between">
         <div class="text-md mt-1">
           {product.category}
         </div>
-        <div
-          class="text-lg text-right flex-grow justify-end mt-1 mr-4 font-bold"
-        >
-          ${product.price}
-        </div>
-        <div>
-          <span class="z-10 absolute flex items-center justify-center w-10 pt-2.5">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="white"
-            >
-              <path
-                d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
-              />
-            </svg>
-          </span>
-
-          <button
-            class="text-lg pl-8 pr-4 py-1 font-bold bg-indigo-500 text-white rounded-full"
-            >ADD TO CART</button
+        <div class="flex">
+          <div
+            class="text-lg text-right mt-1 mr-1 font-bold"
           >
+            ${product.price}
+          </div>
+          <div
+            on:click={handleAddToCart}
+            class="bg-indigo-500 rounded-full flex items-center h-8"
+          >
+            <span
+              class="flex items-center justify-center pt-1 bg-indigo-500 rounded-full w-10"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="white"
+              >
+                <path
+                  d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"
+                />
+              </svg>
+            </span>
+            <button
+              class="text-sm pr-4 py-1 font-bold text-white border-none"
+              >ADD</button
+            >
+          </div>
         </div>
       </div>
     </div>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { search } from '../stores';
+  import { search, cart } from '../stores';
 </script>
 
 <nav class="bg-indigo-500 py-2 md:py-4">
@@ -15,9 +15,7 @@
         alt="LOGO"
         src="https://www.designfreelogoonline.com/wp-content/uploads/2016/07/000749-online-store-logos-design-free-online-E-commerce-cart-logo-maker-02.png"
       />
-      <div
-        class="flex relative items-stretch h-10 sm:h-12"
-      >
+      <div class="flex relative h-10 sm:h-12">
         <span
           class="z-10 h-full absolute flex items-center justify-center w-10 py-3"
           ><svg
@@ -43,10 +41,24 @@
         />
       </div>
     </div>
-    <div
-      class="flex flex-row items-center sm:block hidden"
-    >
-      <h1 class="text-xl px-2 text-white">CART</h1>
+    <div class="px-2 flex items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="white"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+        />
+      </svg>
+      <h1 class="text-xl pr-2 text-white">
+        {$cart.length}
+      </h1>
     </div>
   </div>
 </nav>
