4b0181b71761dee7f8831871938cafc0aebe9247
Carlos Matos <carlosmatos13@gmail.com>
2022-01-30 23:17:51 -0300

    Fix cart

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -54,7 +54,7 @@
     <div class="px-2 flex items-center">
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        class="h-6 w-6"
+        class="w-8"
         fill="none"
         viewBox="0 0 24 24"
         stroke="white"
@@ -66,9 +66,9 @@
           d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
         />
       </svg>
-      <h1 class="text-xl pr-2 text-white">
+      <p class="text-3xl px-2 text-white">
         {$cart.length}
-      </h1>
+      </p>
     </div>
   </div>
 </nav>
