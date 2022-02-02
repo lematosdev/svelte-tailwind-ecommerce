7fc13a8829d7ae4185ffaeda588d0c0e4d51fc5d
Carlos Matos <carlosmatos13@gmail.com>
2022-02-02 09:17:47 -0300

    Remove Person interface, Add CartItem interface

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,13 @@
-export interface Person {
-  firstName: string;
-  lastName: string;
-}
-
 export interface Product {
+  id: string;
   title: string;
   price: number;
   description: string;
   image: string;
   category: string;
+}
+
+export interface CartItem extends Product {
+  quantity: number;
+  total?: number
 }
