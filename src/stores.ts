779e383b3829724a76041faf6ad96d1fe49439a2
Lematos <lematosdev@gmail.com>
2022-02-02 17:13:13 -0300

    Add total amount to stores

--- src/stores.ts
+++ src/stores.ts
@@ -6,11 +6,21 @@ export const products = writable<Product[]>([]);
 export const cart = writable<CartItem[]>([]);
 export const toggleDropdown = writable(false);
 
+export const totalAmount = derived(cart, ($cart) => {
+  return (
+    Math.round(
+      $cart.reduce((acc, item) => acc + item.total, 0) * 100
+    ) / 100
+  );
+});
+
 export const filteredProducts = derived(
   [products, search],
   ([$products, $search]) => {
     return $products.filter((product) =>
-      product.title.toLowerCase().includes($search.toLowerCase())
+      product.title
+        .toLowerCase()
+        .includes($search.toLowerCase())
     );
   }
 );
