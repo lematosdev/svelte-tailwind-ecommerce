69577db02a27d0552af3f6edd57c98ce4f0c378f
Carlos Matos <carlosmatos13@gmail.com>
2022-02-02 09:16:15 -0300

    Remove unused props

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,7 @@
 import App from './App.svelte';
-import type { Person } from './types'
 
 const app = new App({
-	target: document.body,
-	props: {
-		person: {
-			firstName: 'Carlos',
-			lastName: 'Matos'
-		} as Person
-	}
+  target: document.body
 });
 
 export default app;
