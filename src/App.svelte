78b8513d64a249d34c9204b988bb706856a12618
Lematos <lematosdev@gmail.com>
2022-02-02 22:38:26 -0300

    Add spinner

--- src/App.svelte
+++ src/App.svelte
@@ -2,32 +2,33 @@
   import { onMount } from 'svelte';
   import Header from './lib/Header.svelte';
   import Card from './lib/Card.svelte';
+  import Spinner from './lib/Spinner.svelte';
   import { products, filteredProducts } from './stores';
-
+  let loading = true;
   onMount(async () => {
     const res = await fetch(
       'https://fakestoreapi.com/products'
     );
     const data = await res.json();
     $products = data;
+    loading = false;
   });
 </script>
 
 <div>
   <Header />
   <main>
-    <div
-      class="grid md:grid-cols-3 sm:grid-cols-1 grid-cols-1"
-    >
-      {#each $filteredProducts as product}
-        <Card {product} />
-      {/each}
-    </div>
-    <p>
-      Visit the <a href="https://svelte.dev/tutorial"
-        >Svelte tutorial</a
-      > to learn how to build Svelte apps.
-    </p>
+    {#if loading}
+      <Spinner />
+    {:else}
+      <div
+        class="grid md:grid-cols-3 sm:grid-cols-1 grid-cols-1"
+      >
+        {#each $filteredProducts as product}
+          <Card {product} />
+        {/each}
+      </div>
+    {/if}
   </main>
 </div>
 
